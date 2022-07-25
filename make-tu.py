761f159755b59797649a1ec8a083d63278ad7c30
David Kemp <19152940+davaya@users.noreply.github.com>
2022-07-25 16:06:10 -0400

    make transfer unit

--- make-tu.py
+++ make-tu.py
@@ -8,46 +8,42 @@ import os
 
 SCHEMA = 'Schemas/spdx-v3.jidl'
 DATA_DIR = 'Elements'
+TEMPLATE_DIR = DATA_DIR + '/Templates'
 OUTPUT_DIR = 'Out'
 
 
-def load_element(fn: str, codec: jadn.codec.Codec) -> dict:
-    with os.open(fn) as fp:
-        return codec.decode('Element', json.load(fp))
+def read_elements(dirname: str, codec):
+    elements = []
+    with os.scandir(dirname) as it:
+        for entry in it:
+            if entry.is_file() and os.path.splitext(entry.name)[1] == '.json':
+                with open(entry.path) as fp:
+                    element = codec.decode('Element', json.load(fp))
+                elements.append(element)
+    return elements
 
 
 class TransferUnit():
-    def __init__(self,
-                 schema: str = SCHEMA,
-                 data_dir: str = DATA_DIR,
-                 output_dir: str = OUTPUT_DIR,
-                 namespace: str = ''):
 
-        self.foo = False
-        with open(schema) as fp:
+    def make(self, template_file: str = 'baker-a1.json'):
+
+        with open(SCHEMA) as fp:
             schema = jadn.load_any(fp)
-        self.codec = jadn.codec.Codec(schema, verbose_rec=True, verbose_str=True)
-        os.makedirs(output_dir, exist_ok=True)
+        codec = jadn.codec.Codec(schema, verbose_rec=True, verbose_str=True)
+        os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-        self.elements = {}
-        for path in os.scandir(data_dir):
-            if path.is_file() and os.path.splitext(path)[1] == '.json':
-                print(path)
-                with open(path) as fp:
-                    element = self.codec.decode('Element', json.load(fp))
-                self.elements.update(element)
+        elements = read_elements(DATA_DIR, codec)
+        with open(os.path.join([TEMPLATE_DIR, template_file])) as tf:
+            template = json.load(tf)
 
+        tu = {'namespace': template['namespace']}
+        nm = template.get('namespaceMap')
+        tu.update({'namespaceMap': nm} if nm else {})
 
-def make(namespace: str, elements: list[str]):
-    return
-
-
-def split(unit: str):
-    return
+    def split(self, spdx_file: str):
+        return
 
 
 if __name__ == '__main__':
     print(f'Installed JADN version: {jadn.__version__}\n')
-
-    tu = TransferUnit()
-    fire.Fire(tu)
+    fire.Fire(TransferUnit)
